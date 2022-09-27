67340618daa68db0a78c96eee46a7938ac1f4509
rgerm <roland.germ@admiral.at>
2022-09-27 11:56:39 +0200

    refactor: correct typo in name

--- server/src/main/java/com/craftmanship/insurance/model/TaxRequestDTO.java
+++ server/src/main/java/com/craftmanship/insurance/model/TaxRequestDTO.java
@@ -2,5 +2,5 @@ package com.craftmanship.insurance.model;
 
 import java.time.LocalDate;
 
-public record TaxRequestDTO(int co2Emmisions, int power, String fuelType, LocalDate firstRegistration) {
+public record TaxRequestDTO(int co2Emissions, int power, String fuelType, LocalDate firstRegistration) {
 }
