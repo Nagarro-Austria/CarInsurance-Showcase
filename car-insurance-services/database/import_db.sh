31d2771de4a39f4fb0a2d3340a98a9c7f48c08aa
rgerm <roland.germ@admiral.at>
2022-09-30 08:55:29 +0200

    chore: fix CRLF with dos2unix

---
+++ car-insurance-services/database/import_db.sh
@@ -0,0 +1,6 @@
+mysql --host="mysql_server" --user="user_name" --database="database_name" < "path/to/sql/file.sql"
+
+
+mysql --host="mysql_server" --user="user_name" --database="database_name" --password="user_password" < "path/to/sql/file.sql"
+
+mysql -u root -p us_database < us.sql
--- car-insurance-services/database/setup_db.sh
+++ car-insurance-services/database/setup_db.sh
@@ -3,4 +3,5 @@
 docker rm -f mariadb 2&> /dev/null
 
 # start mariadb and launch all sql scripts in the directory database
-docker run -p 3306:3306 --name mariadb -e -d  -v $PWD/database:/docker-entrypoint-initdb.d mariadb:10.4
+docker run -p 3306:3306 --name mariadb -e MARIADB_ALLOW_EMPTY_ROOT_PASSWORD -d mariadb:10.4
+
